a0bb1d4ce65fdb17d96f464d873ee2b4564602d1
Simon MASURIER <simon.masurier@sfr.fr>
2025-04-21 12:19:21 +0200

    Corrections fautes de frappes chemin planning prod

--- projet/Constants.cs
+++ projet/Constants.cs
@@ -12,8 +12,8 @@ namespace projet
         public const string IdNetworkPath_J = @"J:\Logistique et Planning cdes\PLANNING Cdes\identifiants.xlsx";
         //public const string IdNetworkPath_J = @"C:\Users\Simon\Documents\Meerkat\id.xlsx";
 
-        public const string ExcelNetworkPath_P = @"P:\Logistique et Planning cdes\PLANNING Cdes\planning Cdes.xlsx.xlsx";
-        public const string ExcelNetworkPath_J = @"J:\Logistique et Planning cdes\PLANNING Cdes\planning Cdes.xlsx.xlsx";
+        public const string ExcelNetworkPath_P = @"P:\Logistique et Planning cdes\PLANNING Cdes\planning Cdes.xlsx";
+        public const string ExcelNetworkPath_J = @"J:\Logistique et Planning cdes\PLANNING Cdes\planning Cdes.xlsx";
         //public const string ExcelNetworkPath_J = @"C:\Users\Simon\Documents\Meerkat\test.xlsx";
 
     }
